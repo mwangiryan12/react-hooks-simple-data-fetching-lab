46fb1f16432a638fbad50ff8e29b61a42c0f120d
mwangiryan12 <mwangiryan12@gmail.com>
2024-05-18 13:53:46 +0300

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
 // create your App component here
+const { useEffect, useState } = require("react")
+
+// create your App component here
+function App(){
+  
+    const [image,setImage] = useState("")
+    useEffect(()=>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res=>res.json())
+        .then(data=>setImage(data.message));
+      
+    },[])
+
+    return(
+        <div>
+            {!image && <p>Loading...</p>}
+            {image && <img src={image} alt="A Random Dog"/>}
+
+        </div>
+       
+
+    )
+}
+export default App
